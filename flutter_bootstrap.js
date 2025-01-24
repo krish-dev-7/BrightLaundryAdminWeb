845fe6399765e8cb53d4eeaeda42f83f74ffa425
vkrishnasundar <krishnasundar-veeramanikandan-ext@syensqo.com>
2025-01-25 02:04:48 +0530

    V3 corrections

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4179305879"
+    serviceWorkerVersion: "2185384666"
   }
 });
